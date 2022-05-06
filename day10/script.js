8b76b07acc799d6a7290c413c8d7076dc3147162
Truc Vuong - ヴオン　チュック <96474230+trucvuong1503@users.noreply.github.com>
2022-05-06 13:43:43 +0900

    Add files via upload

---
+++ day10/script.js
@@ -0,0 +1,20 @@
+const jokeEL = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+jokeBtn.addEventListener('click', generateJoke)
+generateJoke()
+
+function generateJoke(){
+    const config = {
+
+    headers: {
+            Accept: 'application/json',
+        },
+    }
+
+
+    fetch('https://icanhazdadjoke.com', config)
+        .then((res) => res.json())
+        .then((data) => {
+            jokeEL.innerHTML = data.joke
+        })
+}
